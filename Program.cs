a62f3bb7d02651a51fd57bfebffd204ff3bc5342
Una Árnadóttir <unaemelia@gmail.com>
2024-01-23 17:26:51 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
+
+app.MapGet("/subtract", (int num1, int num2) => SubtractNumbers(num1, num2));
+
+app.Run();
+
+string AddNumbers(int num1, int num2)
+{
+    return $"Summan av {num1} och {num2} är: {num1 + num2}";
+}
+
+string SubtractNumbers(int num1, int num2)
+{
+    return $"Subtraktion av {num1} och {num2} är: {num1 - num2}";
+}
